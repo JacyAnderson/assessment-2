044b5faa154941290a9c3f08e0e744f7e73c274f
JacyAnderson <jacyjamesanderson@gmail.com>
2017-03-26 17:16:32 -0600

    add comments for stress relief and procrastination

--- js/main.js
+++ js/main.js
@@ -9,8 +9,33 @@ window.addEventListener("keydown", checkKeyPressed, false);
 
 
 
-function movePlayer(e) {
-	if (e.keyCode == "65"){
-		
+var roland = document.getElementsByClassName('playerOne');
+
+
+//function that is called in the movePlayerOne function below when the right key gets pressed
+function rightPressed() {
+	console.log(roland);
+	roland.style.left = parseInt(roland.style.left)+ 5 + 'px';
+}
+
+
+//switch function that responds to keydown
+function movePlayerOne(e) {
+	switch (e.keyCode) {
+	  case 37: //left key
+	  	  console.log('left key pressed');
+	    break;
+	  case 38: // up key
+	  	  console.log('up key pressed');
+	  	break;
+	  case 39: // right key
+	  	  rightPressed();
+	      console.log('right key presed');
+	  	break;
+	  case 40: // down key
+	  	  console.log('down key pressed');
+	    break;
 	}
 }
+//listens for the keydown events and tires to run the movePlayerOne function
+window.addEventListener("keydown", movePlayerOne);
