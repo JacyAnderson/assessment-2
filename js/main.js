fc16af61c9fe8a119f6e421618d789ede7560341
JacyAnderson <jacyjamesanderson@gmail.com>
2017-03-26 14:56:11 -0600

    add new .png images with clear backgrounds

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
+// function that loks to console if the "a key is pressed
 function checkKeyPressed(e) {
 	if (e.keyCode == "65") {
 		console.log("The 'a' key is pressed");
 	}
 }
-
+// listens for the a key being pressed 
 window.addEventListener("keydown", checkKeyPressed, false);
+
+
+
+function movePlayer(e) {
+	if (e.keyCode == "65"){
+		
+	}
+}
