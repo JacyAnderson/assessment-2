365f30e2d4d25198d148f816b9ae4824e534f838
JacyAnderson <jacyjamesanderson@gmail.com>
2017-03-26 19:28:01 -0600

    move function working once....

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
 // function that loks to console if the "a key is pressed
-function checkKeyPressed(e) {
-	if (e.keyCode == "65") {
-		console.log("The 'a' key is pressed");
-	}
-}
-// listens for the a key being pressed 
-window.addEventListener("keydown", checkKeyPressed, false);
+
+// function checkKeyPressed(e) {
+// 	if (e.keyCode == "65") {
+// 		console.log("The 'a' key is pressed");
+// 	}
+// }
+// // listens for the a key being pressed 
+// window.addEventListener("keydown", checkKeyPressed, false);
 
 
 
 var roland = document.getElementsByClassName('playerOne');
+console.log(roland);
 
-
-//function that is called in the movePlayerOne function below when the right key gets pressed
-function rightPressed() {
-	console.log(roland);
-	roland.style.left = parseInt(roland.style.left)+ 5 + 'px';
-}
-
+//listens for the keydown events and tries to run the movePlayerOne function
+window.addEventListener("keydown", movePlayerOne);
 
 //switch function that responds to keydown
+
 function movePlayerOne(e) {
 	switch (e.keyCode) {
 	  case 37: //left key
@@ -29,13 +27,16 @@ function movePlayerOne(e) {
 	  	  console.log('up key pressed');
 	  	break;
 	  case 39: // right key
+	  	  console.log('right key presed');
 	  	  rightPressed();
-	      console.log('right key presed');
 	  	break;
 	  case 40: // down key
 	  	  console.log('down key pressed');
 	    break;
 	}
 }
-//listens for the keydown events and tires to run the movePlayerOne function
-window.addEventListener("keydown", movePlayerOne);
+//function that is called in the movePlayerOne function when the right key is pressed
+function rightPressed() {
+	console.log(roland);
+	document.querySelector('.playerOne').style.left = "30px";
+}
