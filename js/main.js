51f51e88cb1d035e2c30f723b0071111bcb4e967
JacyAnderson <jacyjamesanderson@gmail.com>
2017-03-26 20:47:21 -0600

    add some styling because my brain is too shot to figure the js out

--- js/main.js
+++ js/main.js
@@ -14,29 +14,46 @@ var roland = document.getElementsByClassName('playerOne');
 console.log(roland);
 
 //listens for the keydown events and tries to run the movePlayerOne function
-window.addEventListener("keydown", movePlayerOne);
-
 //switch function that responds to keydown
-
-function movePlayerOne(e) {
+window.addEventListener("keydown", function moveWalter(e){
 	switch (e.keyCode) {
 	  case 37: //left key
 	  	  console.log('left key pressed');
 	    break;
 	  case 38: // up key
+	  	  function upPressed() {
+		    document.querySelector('.playerTwo').style.bottom = 30 + "px";
+		  }
+		  upPressed();
 	  	  console.log('up key pressed');
 	  	break;
 	  case 39: // right key
-	  	  console.log('right key presed');
-	  	  rightPressed();
+
 	  	break;
 	  case 40: // down key
 	  	  console.log('down key pressed');
 	    break;
 	}
-}
+});
+window.addEventListener("keydown", function moveRoland(e){
+	switch (e.keyCode) {
+	  case 87: //w key
+	  	function upPressed() {
+		  document.querySelector('.playerOne').style.bottom = 30 + "px";
+		  }
+		 upPressed();
+	  	console.log('w key pressed');
+	    break;
+	  case 65: // a key
+	  	console.log('a key pressed');
+	  	break;
+	  case 68: // d key
+	    console.log('d key pressed');
+	  	break;
+	  case 83: // s key
+	  	  console.log('s key pressed');
+	    break;
+	}
+});
 //function that is called in the movePlayerOne function when the right key is pressed
-function rightPressed() {
-	console.log(roland);
-	document.querySelector('.playerOne').style.left = "30px";
-}
+
