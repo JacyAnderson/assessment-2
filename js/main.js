3cf1a33fd9e9fefe38a6d697650474c498164f2d
JacyAnderson <jacyjamesanderson@gmail.com>
2017-03-27 09:01:34 -0600

    add movement and checkWin function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-const roland = document.getElementsByClassName('playerOne');
+var roland = document.getElementsByClassName('playerOne');
+var walter = document.getElementsByClassName('playerTwo');
+
+var rolandUp = 0;
+var walterUp = 0;
+var moveUp = 5;
 console.log(roland);
 
 /**
@@ -19,7 +24,8 @@ window.addEventListener("keydown", function moveWalter(e) {
 	  // up key
 	  case 38: {
 	  	function upPressed() {
-		  document.querySelector('.playerTwo').style.bottom = 30 + "px";
+		  walterUp = (walterUp + moveUp);
+		  document.querySelector('.playerTwo').style.bottom = walterUp + 'px';
 		}
 		upPressed();
 	  	console.log('up key pressed');
@@ -49,7 +55,9 @@ window.addEventListener("keydown", function moveRoland(e) {
 	  // w key
 	  case 87: {
 	  	function upPressed() {
-		  document.querySelector('.playerOne').style.bottom = 30 + "px";
+		  rolandUp = (rolandUp + moveUp);
+		  document.querySelector('.playerOne').style.bottom = rolandUp + 'px';
+		  checkWin();
 		}
 		upPressed();
 	  	console.log('w key pressed');
@@ -76,4 +84,16 @@ window.addEventListener("keydown", function moveRoland(e) {
 	}
 });
 
+//Win Condition 
+function checkWin() { 
 
+  //check for Roland win
+  if (rolandUp >= 220) {
+  	alert ("Roland has made it to The Dark Tower. The Gunslinger wins!");
+  }
+
+  //check for Walter win
+  else if (walterUp >=220) {
+  	alert ("The Man in Black has made it to the tower. Go then, Gunslinger, there are other worlds than these.");
+  }
+}
